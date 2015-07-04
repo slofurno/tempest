09775417af15c8b6b6bc8609c62196ab2a2b067a
slofuno <slofurno@gmail.com>
2015-07-04 09:52:17 -0400

    f

--- main.go
+++ main.go
@@ -1,12 +1,37 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
+	"strings"
 )
+
+type Mux struct {
+	http.Handler
+}
+
+func (mux Mux) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+
+	domainParts := strings.Split(r.Host, ".")
+	fmt.Println(domainParts)
+
+	//mux.ServeHTTP(w, r)
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, %q", r.URL.Path[1:])
+}
 
 func main() {
 
-	http.Handle("/", http.FileServer(http.Dir("static")))
-	http.ListenAndServe(":555", nil)
+	mux := http.NewServeMux()
+	//mux.HandleFunc("/", handler)
 
+	http.ListenAndServe(":80", mux)
+	mux.HandleFunc("/", handler)
+
+	/*
+		http.Handle("/", http.FileServer(http.Dir("static")))
+		http.ListenAndServe(":555", nil)
+	*/
 }
